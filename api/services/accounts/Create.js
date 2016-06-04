9ec8b4600c9b2159618903f4e6251e1b4d04196a
gudvin <koretskiyav@gmail.com>
2016-06-04 15:55:26 +0300

    change accounts ref way

--- api/services/accounts/Create.js
+++ api/services/accounts/Create.js
@@ -1,23 +1,39 @@
 import { dumpAccount } from 'services/utils';
+import Exception from 'helpers/Exception';
 import Base from 'services/Base';
 import mongoose from 'models';
 
 const Account = mongoose.model('Account');
+const Budget = mongoose.model('Budget');
+const Currency = mongoose.model('Currency');
 
 export default class Create extends Base {
   validate(data) {
     const rules = {
       title: 'required',
-      currencyId: ['required', 'object_id'],
-      budgetId: ['required', 'object_id'],
+      currency: ['required', 'object_id'],
+      user: ['required', 'object_id'],
     };
     return this.validator.validate({ ...data, ...this.context }, rules);
   }
 
-  async execute(data) {
-    const account = new Account(data);
+  async execute({ user, title, currency }) {
+    const budget = await Budget.findOne({ users: user });
 
+    if (!await Currency.findById(currency, { budget: budget.id })) {
+      throw new Exception({
+        code: 'NOT_FOUND',
+        fields: {
+          currency: 'NOT_FOUND',
+        },
+      });
+    }
+
+    const account = new Account({ title, currency, budget: budget.id });
     await account.save();
+
+    budget.accounts.push(account);
+    await budget.save();
 
     return {
       data: dumpAccount(account),
--- api/services/accounts/List.js
+++ api/services/accounts/List.js
@@ -2,21 +2,21 @@ import { dumpAccount } from 'services/utils';
 import Base from 'services/Base';
 import mongoose from 'models';
 
-const Account = mongoose.model('Account');
+const Budget = mongoose.model('Budget');
 
 export default class List extends Base {
   validate() {
     const rules = {
-      budgetId: ['required', 'object_id'],
+      user: ['required', 'object_id'],
     };
     return this.validator.validate(this.context, rules);
   }
 
-  async execute(data) {
-    const accounts = await Account.find(data);
+  async execute({ user }) {
+    const budget = await Budget.findOne({ users: user }).populate('accounts').exec();
 
     return {
-      data: accounts.map(dumpAccount),
+      data: budget.accounts.map(dumpAccount),
     };
   }
 }

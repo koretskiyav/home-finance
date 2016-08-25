ff34acd461487919264067d8cc281d9738fd80d0
Oleg Avgustinov <avgustinov.oleg@gmail.com>
2016-08-25 11:31:26 +0300

    add show different links for auth/no auth users

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
 class NavBar extends Component {
@@ -7,10 +8,17 @@ class NavBar extends Component {
     return (
       <ul>
         <li><Link to='/'>App</Link></li>
-        <li><Link to='/home'>Home</Link></li>
+        {this.props.user
+          ? <li><Link to='/home'>Home</Link></li>
+          : null
+        }
       </ul>
     );
   }
 }
+
+NavBar.propTypes = {
+  user: PropTypes.object,
+};
 
 export default NavBar;

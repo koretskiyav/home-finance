acdf8e03062fe25aa4bda70e93204bf0278d188f
Oleg Avgustinov <avgustinov.oleg@gmail.com>
2016-08-25 12:28:40 +0300

    changed to component stateless

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,17 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-class NavBar extends Component {
-
-  render() {
-    return (
-      <ul>
-        <li><Link to='/'>App</Link></li>
-        {this.props.user
-          ? <li><Link to='/home'>Home</Link></li>
-          : null
-        }
-      </ul>
-    );
-  }
-}
+const { object } = PropTypes;
+const NavBar = ({user}) => (
+  <ul>
+    <li><Link to='/'>App</Link></li>
+    {user ? <li><Link to='/home'>Home</Link></li> : null}
+  </ul>
+)
 
 NavBar.propTypes = {
-  user: PropTypes.object,
+  user: object,
 };
 
 export default NavBar;

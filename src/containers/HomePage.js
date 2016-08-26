7aa6bca7b33a2f9175f7d8ef9947e55bc9a3bdd9
Oleg Avgustinov <avgustinov.oleg@gmail.com>
2016-08-26 12:38:18 +0300

    add editCurrency method

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CurrencyForm from 'components/CurrencyForm';
 import CurrencyLine from 'components/CurrencyLine';
-import { load, add, remove } from 'redux/modules/currencies';
+import { load, add, remove, edit } from 'redux/modules/currencies';
 
 class HomePage extends Component {
   componentWillMount() {
@@ -24,13 +24,24 @@ class HomePage extends Component {
     dispatch(remove({ currencyId }));
   }
 
+  editCurrency = ({ currencyId }) => {
+    const { dispatch } = this.props;
+    dispatch(edit({ currencyId }));
+  }
+
   render() {
     const { user, currencies } = this.props;
     return (
       <div>
         Hello, {user.email} !
         {currencies.data && Object.values(currencies.data).map(currency =>
-          <CurrencyLine key={currency.id} currency={currency} onRemove={this.removeCurrency} />
+          <CurrencyLine
+            key={currency.id}
+            currency={currency}
+            editable={currency.id == currencies.currentEditableValue}
+            onEdit={this.editCurrency}
+            onRemove={this.removeCurrency}
+          />
         )}
         <CurrencyForm onSubmit={this.addCurrency} />
       </div>
